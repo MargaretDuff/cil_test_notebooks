710c88ff623e4b7742b97f4420a2c2bdbdba7cf1
Margaret Duff <margaret.duff@stfc.ac.uk>
2023-08-18 08:21:37 +0000

    Testing the partitioner

--- Partitioner/test_partitioner.py
+++ Partitioner/test_partitioner.py
@@ -1,25 +1,13 @@
 #%%
 from cil.utilities import dataexample
-from cil.framework import DataContainer, BlockDataContainer, Partitioner
 
-# Import libraries
-  
-from cil.optimisation.algorithms import SPDHG
-from cil.optimisation.operators import GradientOperator, BlockOperator
-from cil.optimisation.functions import IndicatorBox, BlockFunction, L2NormSquared, MixedL21Norm
- 
-from cil.io import ZEISSDataReader
- 
-from cil.processors import Slicer, Binner, TransmissionAbsorptionConverter
  
 from cil.plugins.astra.operators import ProjectionOperator
-from cil.plugins.ccpi_regularisation.functions import FGP_TV
+
  
 from cil.utilities.display import show2D
  
- 
-import numpy as np
-import matplotlib.pyplot as plt
+
 import os
 #%%
 data=dataexample.SIMULATED_PARALLEL_BEAM_DATA.get()
@@ -49,27 +37,5 @@ show2D(partitioned_data)
 print(data.shape)
 
 #%%
-# Initialize the lists containing the F_i's and A_i's
-f_subsets = []
 
-
-# Define F_i's 
-for i in range(n_subsets):
-    # Define F_i and put into list
-    fi = 0.5*L2NormSquared(b = partitioned_data[i])
-    f_subsets.append(fi)
-    
-    
-ageom_subset = partitioned_data.geometry
-A = ProjectionOperator(ig2D, ageom_subset)
-
-#F = L2NormSquared.fromBlockDataContainer(partitioned_data, constant=0.5)
-
-# Define F and K
-F = BlockFunction(*f_subsets)
-K = A
-
-# Define G (by default the positivity constraint is on)
-alpha = 0.025
-G = alpha * FGP_TV()
 # %%
